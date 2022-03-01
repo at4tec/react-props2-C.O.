ac8959376be7f3ebf11f4e80026757daa153aa31
at4tec <at4tec@gmail.com>
2022-03-01 15:46:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-yuqze5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-yuqze5)
---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React,{Component} from 'react';
+
+ class App extends Component {
+  render(){
+   return (
+    <div className="App">
+     <Great name='Ahmed' hero='myself'/>
+     <Great name ='Aly' hero='superman'/>
+     <Great name='Ramy' hero='batman'/>
+     from function component props.
+     <br/><br/><br/>
+     <Welcome name='Ahmed' hero='myself'/>
+     <Welcome name ='Aly' hero='superman'/>
+     <Welcome name='Ramy' hero='batman'/>
+     from class component props.
+
+    </div>
+  );
+}
+}
+
+//child1
+class Welcome extends Component{
+  render(){
+      return <h4>
+        Welcome {this.props.name} known as {this.props.hero}.
+        </h4>
+  }
+}
+
+//child2
+function Great (prop){
+  //console.log(prop)
+  return <h4>
+    Hello {prop.name} also K.A. {prop.hero}.
+    </h4>
+}
+
+
+
+export default App;
